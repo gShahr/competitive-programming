323cd207bbb06ca81456078f412d0ea8d62683d5
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-03-17 15:27:19 -0400

    initialize repo

---
+++ codeforces/practice/1500/test.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
---
+++ codeforces/practice/1500/test2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void sol();
+
+int main() {
+    int t = 1;
+    cin >> t;
+    for (int i = 0; i < t; i++) sol();
+    return 0;
+}
+
+void sol() {
+}
