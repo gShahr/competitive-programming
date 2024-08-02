fbc548ef95e076e32d5b1704bb304f787e258864
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-08-02 13:05:24 -0400

    chore: Add missing output in sol() function

--- codeforces/practice/1500/test2.cpp
+++ codeforces/practice/1500/test2.cpp
@@ -24,4 +24,5 @@ int main() {
 }
 
 void sol() {
+    cout << "a" << endl;
 }
---
+++ debugging-template/debug.cpp
@@ -0,0 +1,324 @@
+#ifndef DEBUG_TEMPLATE_CPP
+#define DEBUG_TEMPLATE_CPP
+//#include <bits/stdc++.h>
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+// #define cerr cout
+namespace __DEBUG_UTIL__
+{
+    /* Primitive Datatypes Print */
+    void print(const char *x) { cerr << x; }
+    void print(bool x) { cerr << (x ? "T" : "F"); }
+    void print(char x) { cerr << '\'' << x << '\''; }
+    void print(signed short int x) { cerr << x; }
+    void print(unsigned short int x) { cerr << x; }
+    void print(signed int x) { cerr << x; }
+    void print(unsigned int x) { cerr << x; }
+    void print(signed long int x) { cerr << x; }
+    void print(unsigned long int x) { cerr << x; }
+    void print(signed long long int x) { cerr << x; }
+    void print(unsigned long long int x) { cerr << x; }
+    void print(float x) { cerr << x; }
+    void print(double x) { cerr << x; }
+    void print(long double x) { cerr << x; }
+    void print(string x) { cerr << '\"' << x << '\"'; }
+    template <size_t N>
+    void print(bitset<N> x) { cerr << x; }
+    void print(vector<bool> v)
+    { /* Overloaded this because stl optimizes vector<bool> by using
+          _Bit_reference instead of bool to conserve space. */
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : v)
+            cerr << (f++ ? "," : "") << (i ? "T" : "F");
+        cerr << "}";
+    }
+    /* Templates Declarations to support nested datatypes */
+    template <typename T>
+    void print(T &&x);
+    template <typename T>
+    void print(vector<vector<T>> mat);
+    template <typename T, size_t N, size_t M>
+    void print(T (&mat)[N][M]);
+    template <typename F, typename S>
+    void print(pair<F, S> x);
+    template <typename T, size_t N>
+    struct Tuple;
+    template <typename T>
+    struct Tuple<T, 1>;
+    template <typename... Args>
+    void print(tuple<Args...> t);
+    template <typename... T>
+    void print(priority_queue<T...> pq);
+    template <typename T>
+    void print(stack<T> st);
+    template <typename T>
+    void print(queue<T> q);
+    /* Template Datatypes Definitions */
+    template <typename T>
+    void print(T &&x)
+    {
+        /*  This works for every container that supports range-based loop
+            i.e. vector, set, map, oset, omap, dequeue */
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : x)
+            cerr << (f++ ? "," : ""), print(i);
+        cerr << "}";
+    }
+    template <typename T>
+    void print(vector<vector<T>> mat)
+    {
+        int f = 0;
+        cerr << "\n~~~~~\n";
+        for (auto &&i : mat)
+        {
+            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+        }
+        cerr << "~~~~~\n";
+    }
+    template <typename T, size_t N, size_t M>
+    void print(T (&mat)[N][M])
+    {
+        int f = 0;
+        cerr << "\n~~~~~\n";
+        for (auto &&i : mat)
+        {
+            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+        }
+        cerr << "~~~~~\n";
+    }
+    template <typename F, typename S>
+    void print(pair<F, S> x)
+    {
+        cerr << '(';
+        print(x.first);
+        cerr << ',';
+        print(x.second);
+        cerr << ')';
+    }
+    template <typename T, size_t N>
+    struct Tuple
+    {
+        static void printTuple(T t)
+        {
+            Tuple<T, N - 1>::printTuple(t);
+            cerr << ",", print(get<N - 1>(t));
+        }
+    };
+    template <typename T>
+    struct Tuple<T, 1>
+    {
+        static void printTuple(T t) { print(get<0>(t)); }
+    };
+    template <typename... Args>
+    void print(tuple<Args...> t)
+    {
+        cerr << "(";
+        Tuple<decltype(t), sizeof...(Args)>::printTuple(t);
+        cerr << ")";
+    }
+    template <typename... T>
+    void print(priority_queue<T...> pq)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!pq.empty())
+            cerr << (f++ ? "," : ""), print(pq.top()), pq.pop();
+        cerr << "}";
+    }
+    template <typename T>
+    void print(stack<T> st)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!st.empty())
+            cerr << (f++ ? "," : ""), print(st.top()), st.pop();
+        cerr << "}";
+    }
+    template <typename T>
+    void print(queue<T> q)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!q.empty())
+            cerr << (f++ ? "," : ""), print(q.front()), q.pop();
+        cerr << "}";
+    }
+    /* Printer functions */
+    void printer(const char *) {} /* Base Recursive */
+    template <typename T, typename... V>
+    void printer(const char *names, T &&head, V &&...tail)
+    {
+        /* Using && to capture both lvalues and rvalues */
+        int i = 0;
+        for (size_t bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket != 0); i++)
+            if (names[i] == '(' or names[i] == '<' or names[i] == '{')
+                bracket++;
+            else if (names[i] == ')' or names[i] == '>' or names[i] == '}')
+                bracket--;
+        cerr.write(names, i) << " = ";
+        print(head);
+        if (sizeof...(tail))
+            cerr << " ||", printer(names + i + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+    /* PrinterArr */
+    void printerArr(const char *) {} /* Base Recursive */
+    template <typename T, typename... V>
+    void printerArr(const char *names, T arr[], size_t N, V... tail)
+    {
+        size_t ind = 0;
+        for (; names[ind] and names[ind] != ','; ind++)
+            cerr << names[ind];
+        for (ind++; names[ind] and names[ind] != ','; ind++)
+            ;
+        cerr << " = {";
+        for (size_t i = 0; i < N; i++)
+            cerr << (i ? "," : ""), print(arr[i]);
+        cerr << "}";
+        if (sizeof...(tail))
+            cerr << " ||", printerArr(names + ind + 1, tail...);
+        else
+            cerr << "]\n";
+    }
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define debugArr(...) cerr << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+#endif
+
+#define int long long int
+
+void sol();
+
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    for (int i = 0; i < t; i++) sol();
+    return 0;
+}
+
+void sol() {
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int l = 0;
+    int resetL = 0;
+    int sum = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (a[i] > x) {
+            l = i+1;
+            resetL = i;
+            sum = 0;
+        } else {
+            sum += a[i];
+            while (sum > x) {
+                sum -= a[l];
+                resetL = l;
+                l++;
+            }
+            int sum2 = sum - a[i] + a[resetL];
+            while (resetL-1 >= 0 && sum2 <= x) {
+                sum2 += a[resetL-1];
+                resetL--;
+            }
+            if (sum2 <= x) resetL = -1;
+            ans += (i-l+1) + resetL+1;
+            // cout << i << ' ' << resetL << endl;
+        }
+    }
+    cout << ans << endl;
+}
