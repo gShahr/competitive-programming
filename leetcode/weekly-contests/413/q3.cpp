0a214524a33ef8628468bdc86eae217e4ebd3fcd
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-09-03 21:06:34 -0400

    Add new idea

--- leetcode/weekly-contests/413/q3.cpp
+++ leetcode/weekly-contests/413/q3.cpp
@@ -1,7 +1,3 @@
-bool solve(int i, int j) {
-    solve(i-1, j)
-}
-
 class Solution {
 public:
     int maxScore(vector<vector<int>>& grid) {
@@ -12,22 +8,10 @@ public:
         memset(dp, 0, sizeof(dp));
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                for (int k = 1; k <= m; k++) {
-                    // check dp[i-1][k][i]
-                    bool ok = true;
-                    for (int l = 1; l <= mx; l++) {
-                        if (dp[i-1][k][l] == grid[i-1][j-1]) ok = false;
-                    }
-                    int sum = 0;
-                    if (ok) sum += grid[i-1][j-1];
-                    for (int l = 1; l <= mx; l++) sum += dp[i-1][k][l];
-                    int currSum = 0;
-                    for (int l = 1; l <= mx; l++) currSum += dp[i][j][l];
-                    if (sum > currSum) {
-                        for (int l = 1; l <= mx; l++) dp[i][j][l] = 0;
-                        for (int l = 1; l <= mx; l++) dp[i][j][l] = dp[i-1][k][l];
-                        dp[i][j][grid[i-1][j-1]] = grid[i-1][j-1];
-                    }
+                int mxV = dp[i-1][j][grid[i-1][j-1]] + grid[i-1][j-1];
+                for (int l = 1; l <= mx; l++) {
+                    if (l == grid[i-1][j-1]) continue;
+                    dp[i][j][l] = max(dp[i][j][l], mxV);
                 }
             }
         }
@@ -61,5 +45,7 @@ taken values
 curr cell
 
 greatest sum that doesn't contain grid[i][j]
+
+dp[i][j][k] := the max amount that doesn't use value k
 
 */
