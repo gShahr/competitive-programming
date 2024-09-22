42e28eba15cd656bc3ec8850427a10efff3f7dcb
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-09-22 00:04:46 -0400

    Add correct solution
    Needed to define bounds checking for l-index because r index is always fixed to be the boundary + 1 and doesn't change once it hits n.

--- leetcode/weekly-contests/416/q3.cpp
+++ leetcode/weekly-contests/416/q3.cpp
@@ -12,10 +12,12 @@ public:
         int n = word1.size();
         int m = word2.size();
         map<char, int> cnt1, cnt2;
+        for (char i = 'a'; i <= 'z'; i++) cnt1[i] = 0;
+        for (char i = 'a'; i <= 'z'; i++) cnt2[i] = 0;
         for (auto i: word2) cnt2[i]++;
         int l = 0;
         int r = 0;
-        while (r < n) {
+        while (l < n && r <= n) {
             while (r < n && !check(cnt1, cnt2)) {
                 cnt1[word1[r]]++;
                 r++;
