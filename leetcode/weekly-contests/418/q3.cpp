2d7376be5f7bf420f70ff1a3aae069d8e291cee0
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-10-05 23:43:54 -0400

    Add notes for 3rd question

---
+++ leetcode/weekly-contests/418/q3.cpp
@@ -0,0 +1,15 @@
+/*
+
+find smallest degree of node 
+
+shape of the grid?
+elements that go into them
+
+assume that the shape of the grid is known
+=> assume all nodes have degree greater than 5
+
+8 6 5
+7 3 2
+1 0 
+
+*/
