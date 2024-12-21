2e246dd4821bc78385796c1311585861762e1b0c
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-12-21 12:43:38 -0500

    Add solution for problem F in codeforces contest 2050 with segment tree implementation for range GCD queries

---
+++ codeforces/practice/2050/F.cpp
@@ -0,0 +1,387 @@
+#ifndef DEBUG_TEMPLATE_CPP
+#define DEBUG_TEMPLATE_CPP
+//#include <bits/stdc++.h>
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+// #define cerr cout
+namespace __DEBUG_UTIL__
+{
+    using namespace std;
+    bool I_want_colored_output = true; /* ONLY WORKS WITH TERMINAL */
+    string white = I_want_colored_output ? "\033[0;m" : "";
+    string outer = I_want_colored_output ? "\033[0;31m" : "";    // red
+    string varName = I_want_colored_output ? "\033[1;34m" : "";  // blue
+    string varValue = I_want_colored_output ? "\033[1;32m" : ""; // green
+
+    /* Primitive Datatypes Print */
+    void print(const char *x) { cerr << x; }
+    void print(bool x) { cerr << (x ? "T" : "F"); }
+    void print(char x) { cerr << '\'' << x << '\''; }
+    void print(signed short int x) { cerr << x; }
+    void print(unsigned short int x) { cerr << x; }
+    void print(signed int x) { cerr << x; }
+    void print(unsigned int x) { cerr << x; }
+    void print(signed long int x) { cerr << x; }
+    void print(unsigned long int x) { cerr << x; }
+    void print(signed long long int x) { cerr << x; }
+    void print(unsigned long long int x) { cerr << x; }
+    void print(float x) { cerr << x; }
+    void print(double x) { cerr << x; }
+    void print(long double x) { cerr << x; }
+    void print(string x) { cerr << '\"' << x << '\"'; }
+    template <size_t N>
+    void print(bitset<N> x) { cerr << x; }
+    void print(vector<bool> v)
+    { /* Overloaded this because stl optimizes vector<bool> by using
+          _Bit_reference instead of bool to conserve space. */
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : v)
+            cerr << (f++ ? "," : "") << (i ? "T" : "F");
+        cerr << "}";
+    }
+    /* Templates Declarations to support nested datatypes */
+    template <typename T>
+    void print(T &&x);
+    template <typename T>
+    void print(vector<vector<T>> mat);
+    template <typename T, size_t N, size_t M>
+    void print(T (&mat)[N][M]);
+    template <typename F, typename S>
+    void print(pair<F, S> x);
+    template <typename T, size_t N>
+    struct Tuple;
+    template <typename T>
+    struct Tuple<T, 1>;
+    template <typename... Args>
+    void print(tuple<Args...> t);
+    template <typename... T>
+    void print(priority_queue<T...> pq);
+    template <typename T>
+    void print(stack<T> st);
+    template <typename T>
+    void print(queue<T> q);
+    /* Template Datatypes Definitions */
+    template <typename T>
+    void print(T &&x)
+    {
+        /*  This works for every container that supports range-based loop
+            i.e. vector, set, map, oset, omap, dequeue */
+        int f = 0;
+        cerr << '{';
+        for (auto &&i : x)
+            cerr << (f++ ? "," : ""), print(i);
+        cerr << "}";
+    }
+    template <typename T>
+    void print(vector<vector<T>> mat)
+    {
+        int f = 0;
+        cerr << "\n~~~~~\n";
+        for (auto &&i : mat)
+        {
+            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+        }
+        cerr << "~~~~~\n";
+    }
+    template <typename T, size_t N, size_t M>
+    void print(T (&mat)[N][M])
+    {
+        int f = 0;
+        cerr << "\n~~~~~\n";
+        for (auto &&i : mat)
+        {
+            cerr << setw(2) << left << f++, print(i), cerr << "\n";
+        }
+        cerr << "~~~~~\n";
+    }
+    template <typename F, typename S>
+    void print(pair<F, S> x)
+    {
+        cerr << '(';
+        print(x.first);
+        cerr << ',';
+        print(x.second);
+        cerr << ')';
+    }
+    template <typename T, size_t N>
+    struct Tuple
+    {
+        static void printTuple(T t)
+        {
+            Tuple<T, N - 1>::printTuple(t);
+            cerr << ",", print(get<N - 1>(t));
+        }
+    };
+    template <typename T>
+    struct Tuple<T, 1>
+    {
+        static void printTuple(T t) { print(get<0>(t)); }
+    };
+    template <typename... Args>
+    void print(tuple<Args...> t)
+    {
+        cerr << "(";
+        Tuple<decltype(t), sizeof...(Args)>::printTuple(t);
+        cerr << ")";
+    }
+    template <typename... T>
+    void print(priority_queue<T...> pq)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!pq.empty())
+            cerr << (f++ ? "," : ""), print(pq.top()), pq.pop();
+        cerr << "}";
+    }
+    template <typename T>
+    void print(stack<T> st)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!st.empty())
+            cerr << (f++ ? "," : ""), print(st.top()), st.pop();
+        cerr << "}";
+    }
+    template <typename T>
+    void print(queue<T> q)
+    {
+        int f = 0;
+        cerr << '{';
+        while (!q.empty())
+            cerr << (f++ ? "," : ""), print(q.front()), q.pop();
+        cerr << "}";
+    }
+    /* Printer functions */
+    void printer(const char *) {} /* Base Recursive */
+    template <typename T, typename... V>
+    void printer(const char *names, T &&head, V &&...tail)
+    {
+        /* Using && to capture both lvalues and rvalues */
+        int i = 0;
+        for (size_t bracket = 0; names[i] != '\0' and (names[i] != ',' or bracket != 0); i++)
+            if (names[i] == '(' or names[i] == '<' or names[i] == '{')
+                bracket++;
+            else if (names[i] == ')' or names[i] == '>' or names[i] == '}')
+                bracket--;
+        cerr << varName;
+        cerr.write(names, i) << outer << " = " << varValue;
+        print(head);
+        if (sizeof...(tail))
+            cerr << outer << " ||", printer(names + i + 1, tail...);
+        else
+            cerr << outer << "]\n"
+                 << white;
+    }
+    /* PrinterArr */
+    void printerArr(const char *) {} /* Base Recursive */
+    template <typename T, typename... V>
+    void printerArr(const char *names, T arr[], size_t N, V... tail)
+    {
+        size_t ind = 0;
+        cerr << varName;
+        for (; names[ind] and names[ind] != ','; ind++)
+            cerr << names[ind];
+        for (ind++; names[ind] and names[ind] != ','; ind++)
+            ;
+        cerr << outer << " = " << varValue << "{";
+        for (size_t i = 0; i < N; i++)
+            cerr << (i ? "," : ""), print(arr[i]);
+        cerr << "}";
+        if (sizeof...(tail))
+            cerr << outer << " ||", printerArr(names + ind + 1, tail...);
+        else
+            cerr << outer << "]\n"
+                 << white;
+    }
+}
+#ifndef ONLINE_JUDGE
+#define debug(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << ": [", __DEBUG_UTIL__::printer(#__VA_ARGS__, __VA_ARGS__)
+#define debugArr(...) std::cerr << __DEBUG_UTIL__::outer << __LINE__ << ": [", __DEBUG_UTIL__::printerArr(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+#endif
+
+#define int long long int
+
+// https://www.geeksforgeeks.org/gcds-of-a-given-index-ranges-in-an-array/
+int* st;
+/*  A recursive function to get gcd of given
+    range of array indexes. The following are parameters for
+    this function.
+ 
+    st    --> Pointer to segment tree
+    si --> Index of current node in the segment tree.
+   Initially 0 is passed as root is always at index 0 ss &
+   se  --> Starting and ending indexes of the segment
+                 represented by current node, i.e.,
+   st[index] qs & qe  --> Starting and ending indexes of
+   query range */
+int findGcd(int ss, int se, int qs, int qe, int si)
+{
+    if (ss > qe || se < qs)
+        return 0;
+    if (qs <= ss && qe >= se)
+        return st[si];
+    int mid = ss + (se - ss) / 2;
+    return gcd(findGcd(ss, mid, qs, qe, si * 2 + 1),
+                 findGcd(mid + 1, se, qs, qe, si * 2 + 2));
+}
+ 
+// Finding The gcd of given Range
+int findRangeGcd(int ss, int se, int arr[], int n)
+{
+    if (ss < 0 || se > n - 1 || ss > se) {
+        cout << "Invalid Arguments"
+             << "\n";
+        return -1;
+    }
+    return findGcd(0, n - 1, ss, se, 0);
+}
+ 
+// A recursive function that constructs Segment Tree for
+// array[ss..se]. si is index of current node in segment
+// tree st
+int constructST(int arr[], int ss, int se, int si)
+{
+    if (ss == se) {
+        st[si] = arr[ss];
+        return st[si];
+    }
+    int mid = ss + (se - ss) / 2;
+    st[si]
+        = gcd(constructST(arr, ss, mid, si * 2 + 1),
+                constructST(arr, mid + 1, se, si * 2 + 2));
+    return st[si];
+}
+ 
+/* Function to construct segment tree from given array.
+   This function allocates memory for segment tree and
+   calls constructSTUtil() to fill the allocated memory */
+int* constructSegmentTree(int arr[], int n)
+{
+    int height = (int)(ceil(log2(n)));
+    int size = 2 * (int)pow(2, height) - 1;
+    st = new int[size];
+    constructST(arr, 0, n - 1, 0);
+    return st;
+}
+
+int32_t main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, q;
+        cin >> n >> q;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+        vector<int> diff(n-1);
+        for (int i = 0; i < n-1; i++) diff[i] = abs(a[i] - a[i+1]);
+        if (!diff.empty()) constructSegmentTree(diff.data(), diff.size());
+        for (int i = 0; i < q; i++) {
+            int l, r;
+            cin >> l >> r;
+            l--, r--;
+            int ans = -1;
+            if (l == r) ans = 0;
+            else ans = findRangeGcd(l, r-1, diff.data(), diff.size());
+            debug(ans);
+            cout << ans << ' ';
+        }
+        cout << endl;
+        // delete[] st;
+    }
+}
+
+/*
+
+
+
+*/
